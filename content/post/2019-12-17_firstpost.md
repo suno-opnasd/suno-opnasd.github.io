314c7cb6101d9562ab85cd729f79bea311f2b4f8
Suno <8435528+suno-opnasd@users.noreply.github.com>
2019-12-17 23:15:45 +0900

    append Flex theme

---
+++ content/post/2019-12-17_firstpost.md
@@ -0,0 +1,39 @@
+Title: firstpost
+Date: 2019-12-17 10:38
+Tags: github, pelican, blog
+Author: suno-opnasd
+Summary: first post
+
+# suno-opnasd first page
+
+# 見出し1
+
+## 見出し２
+
+見出し２に対しての本文
+
+### 見出し３
+
+見出し３に対しての本文
+
+### リストとコードブロック
+
+- 項目リスト1
+- 項目リスト2
+- 項目リスト3
+
+何か挟まないと項目リストが解除されない
+
+1. 番号リスト1
+2. 番号リスト2
+3. 番号リスト3
+
+何か挟まないと番号リストが解除されない
+
+- Note with SyntaxHighlight
+
+```python
+print "Hello suno-opnasd!!"
+print "olleH pseudo-ouns!!"
+```
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,13 +4,25 @@ from __future__ import unicode_literals
 
 AUTHOR = 'suno-opnasd'
 SITENAME = 'suno-opnasd github pages'
-SITEURL = ''
+SITEURL = 'https://suno-opnasd.github.io'
 
 PATH = 'content'
 
 TIMEZONE = 'Asia/Tokyo'
+THEME = './pelican-theme/Flex'
 
 DEFAULT_LANG = 'ja'
+DEFAULT_CATEGORY = 'post'
+
+FILENAME_METADATA = r'(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
+MARKDOWN = {
+    'extension_configs': {
+        'markdown.extensions.codehilite': {'css_class': 'highlight'},
+        'markdown.extensions.extra': {},
+        'markdown.extensions.meta': {},
+    },
+    'output_format': 'html5',
+}
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -23,13 +35,13 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         )
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/suno-opnasd'),
+         )
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
