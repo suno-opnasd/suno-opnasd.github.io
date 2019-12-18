63bb9695404dc221c014a59968364da22e6ad0f7
suno-opnasd <8435528+suno-opnasd@users.noreply.github.com>
2019-12-18 10:43:40 +0900

    cusotomizing pelican conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from datetime import datetime
 
 AUTHOR = 'suno-opnasd'
 SITENAME = 'suno-opnasd github pages'
 SITEURL = 'https://suno-opnasd.github.io'
+SITETITLE = 'Portal'
+SITESUBTITLE = 'Suno with Pelican'
+SITE_DESCRIPTION = 'hogeee'
+PYGMENTS_STYLE = 'github-dark'
+
+ROBOTS = 'index, follow'
 
 PATH = 'content'
+# using Flex theme. 
+# see also about custom settings -> https://github.com/alexandrevicenzi/Flex/wiki/Custom-Settings
+THEME = './pelican-theme/Flex'
+
 
 TIMEZONE = 'Asia/Tokyo'
-THEME = './pelican-theme/Flex'
 
 DEFAULT_LANG = 'ja'
 DEFAULT_CATEGORY = 'post'
@@ -39,9 +49,31 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 
 # Social widget
 SOCIAL = (('github', 'https://github.com/suno-opnasd'),
+          ('rss', '/feeds/all.atom.xml'),
          )
 
 DEFAULT_PAGINATION = 10
 
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
+MENUITEMS = (('Posts', '/posts.html'),
+             ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
+
+CC_LICENSE = {
+    'name': 'Creative Commons Attribution-ShareAlike',
+    'version': '4.0',
+    'slug': 'by-sa'
+}
+
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+COPYRIGHT_NAME = AUTHOR
+COPYRIGHT_YEAR = datetime.now().year
+GITHUB_CORNER_URL = 'https://github.com/suno-opnasd/suno-opnasd.gihub.io'
+STATIC_PATHS = ['images', 'extra']
+CUSTOM_CSS = 'static/custom.css'
+#GOOGLE_ADSENSE = {
+#
+#}
